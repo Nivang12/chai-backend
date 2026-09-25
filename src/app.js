9a10d0fe486fb68daef0dad6010f5e0788de7b3d
Nivang <mistrynivang@gmail.com>
2026-09-25 18:23:15 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ app.use(cors({
   app.use(express.static("public"))
   app.use(cookieParser())
   
+//routes import
+import userRouter from './routes/user.router.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
 export {app}
